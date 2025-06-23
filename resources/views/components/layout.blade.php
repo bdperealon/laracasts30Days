e3b47be058d286c1f51456d33ad0d5acd1388a40
bdperealon <perezalonsodamian@gmail.com>
2025-06-23 19:07:52 +0200

    Third lessons complete!!

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,19 +1,46 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>01000100010000010101001001010101</title>
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body>
-        <nav>
-            <x-nav-link href="/">Home</x-nav-link>
-            <x-nav-link href="/about">About</x-nav-link>
-            <x-nav-link href="/contact">Contact</x-nav-link>
-        </nav>
+    <body class="h-full">
+        <div class="min-h-full">
+            <nav class="bg-gray-800">
+                <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                    <div class="flex h-16 items-center justify-between">
+                        <div class="flex items-center">
+                            <div class="shrink-0">
+                                <img class="size-8" src="https://laracasts.com/images/logo/logo-triangle.svg" alt="Your Company" />
+                            </div>
+                            <div class="hidden md:block">
+                                <div class="ml-10 flex items-baseline space-x-4">
+                                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                                    <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
+                                    <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
+                                    <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
 
-        {{ $slot }}
+            <header class="bg-white shadow-sm">
+                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+                </div>
+            </header>
+            <main>
+                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    {{ $slot }}
+                </div>
+            </main>
+        </div>
+
     </body>
 </html>
--- resources/views/components/nav-link.blade.php
+++
@@ -1,3 +0,0 @@
-<a href="{{ $attributes->get('href') }}">
-    {{ $slot }}
-</a>
